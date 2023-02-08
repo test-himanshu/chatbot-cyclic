a304a9cd053d8c4fc16900baac3c4f0b449fa179
unknown <vs6838635@gmail.com>
2023-02-08 18:06:03 +0530

    update

--- server/controller/chatBotController.js
+++ server/controller/chatBotController.js
@@ -107,7 +107,7 @@ function callSendAPI(sender_psid, response){
     
     //send the http request to the messenger platform
     request({
-        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "uri": "https://graph.facebook.com/v16.0/me/messages",
         "qs": { "access_token": process.env.FB_PAGE_TOKEN},
         "method": "POST",
         "json": request_body 
