c06e3aae88904671100d42cfe18494c2b91e2cc4
unknown <vs6838635@gmail.com>
2023-02-08 17:23:58 +0530

    update

--- server/controller/chatBotController.js
+++ server/controller/chatBotController.js
@@ -24,6 +24,7 @@ let postWebhook = (req, res) => {
            //check if the event is a message or postback and
            //pass the event to the appropriate handler function
            if(webhook_event.message){
+            console.log(sender_psid, webhook_event.message);
             handleMessage(sender_psid, webhook_event.message);
            }
            else if(webhook_event.postback){
